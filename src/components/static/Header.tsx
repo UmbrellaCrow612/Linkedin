3e23117361ccd3a6f75c64390ecb2cd741500764
yousaf wazir <umbrellaman612@gmail.com>
2022-07-08 09:17:01 +0100

    ci: made mobile header

--- src/components/static/Header.tsx
+++ src/components/static/Header.tsx
@@ -1,9 +1,18 @@
+import { AiOutlineMenu } from 'react-icons/ai'
+import { Button } from '@material-tailwind/react'
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = () => {
   return (
     <>
-      <nav>navigation</nav>
+      {/** Mobile first */}
+      <nav className="h-[80px] flex items-center px-4 justify-between">
+        <AiOutlineMenu className="text-2xl cursor-pointer" />
+        <div className="w-[200px] flex justify-between items-center">
+          <Button variant="outlined">Sign up</Button>
+          <Button variant="filled">Sign In</Button>
+        </div>
+      </nav>
     </>
   )
 }
