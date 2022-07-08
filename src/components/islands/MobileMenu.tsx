8cc68f9eb75990d7439f045019941f369d6b1973
yousaf wazir <umbrellaman612@gmail.com>
2022-07-08 09:49:13 +0100

    ci: moved mobile menu into islands

---
+++ src/components/islands/MobileMenu.tsx
@@ -0,0 +1,13 @@
+import { AiOutlineMenu } from 'react-icons/ai'
+
+interface MobileMenuProps {
+
+}
+
+export const MobileMenu: React.FC<MobileMenuProps> = () => {
+        return (
+          <>
+            <AiOutlineMenu className="text-2xl cursor-pointer" />
+          </>
+        )
+}
--- src/components/static/Header.tsx
+++ src/components/static/Header.tsx
@@ -1,5 +1,5 @@
-import { AiOutlineMenu } from 'react-icons/ai'
 import { Button } from '@material-tailwind/react'
+import { MobileMenu } from '../islands/MobileMenu'
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = () => {
@@ -7,7 +7,7 @@ export const Header: React.FC<HeaderProps> = () => {
     <>
       {/** Mobile first */}
       <nav className="h-[80px] flex items-center px-4 justify-between">
-        <AiOutlineMenu className="text-2xl cursor-pointer" />
+        <MobileMenu />
         <div className="w-[200px] flex justify-between items-center">
           <Button variant="outlined">Sign up</Button>
           <Button variant="filled">Sign In</Button>
