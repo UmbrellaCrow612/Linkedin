a4107821b8775fb4c4839cd4e610dfeb67b9c7a8
yousaf wazir <umbrellaman612@gmail.com>
2022-07-08 08:49:48 +0100

    ci: added dark mode spinner

--- src/components/islands/LoadingSpinner.tsx
+++ src/components/islands/LoadingSpinner.tsx
@@ -3,8 +3,8 @@ interface LoadingSpinnerProps {}
 export const LoadingSpinner: React.FC<LoadingSpinnerProps> = () => {
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin"></div>
+      <div className="flex items-center justify-center min-h-screen dark:bg-[black]">
+        <div className="w-40 h-40 border-t-4 border-b-4 border-green-900 dark:border-white rounded-full animate-spin"></div>
       </div>
     </>
   )
