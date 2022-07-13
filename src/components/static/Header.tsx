991cc96edcd0703e98b84115ee5e60ac403dc9c4
yousaf wazir <umbrellaman612@gmail.com>
2022-07-13 18:00:27 +0100

    feat: added changeable header based on url

--- src/components/static/Header.tsx
+++ src/components/static/Header.tsx
@@ -3,9 +3,9 @@ import { MobileMenu } from '../islands/MobileMenu'
 import { AiOutlineCompass } from 'react-icons/ai'
 import { BsPeople, BsBook } from 'react-icons/bs'
 import { MdOutlineBusinessCenter } from 'react-icons/md'
-import { useTheme } from 'next-themes'
+import { useRouter } from 'next/router'
 import Image from 'next/image'
-
+import { useEffect, useState } from 'react'
 interface HeaderProps {}
 const options = [
   {
@@ -26,47 +26,58 @@ const options = [
   },
 ]
 const Header: React.FC<HeaderProps> = () => {
-  const { theme } = useTheme()
+  const router = useRouter()
+  const [isFeedPage, setIsFeedPage] = useState(false)
+  useEffect(() => {
+    if (router.pathname === '/feed') setIsFeedPage(true)
+  }, [router.pathname])
 
   return (
     <>
-      <nav className="h-[80px] rounded-xl z-10 dark:bg-[#1b1b1b] bg-white bg-opacity-70 flex items-center px-4 justify-between max-w-screen-2xl mx-auto sticky top-0">
-        {/** Logo dark*/}
-        <div className="hidden dark:md:block">
-          <Image
-            height={35}
-            width={110}
-            src="/images/darkMode-logo.png"
-            alt="Linkedin blue logo"
-          />
-        </div>
-        <div className="hidden dark:hidden md:block">
-          <Image
-            height={65}
-            width={110}
-            src="/images/logo.png"
-            alt="Linkedin blue logo"
-          />
-        </div>
-        {/** Mobile Menu */}
-        <MobileMenu />
-        {/** Desktop options */}
-        <div className="flex items-center md:w-[500px] justify-between">
-          {options.map((option) => (
-            <div
-              key={option.title}
-              className="flex-col items-center justify-center hidden cursor-pointer hover:text-grey-700 md:flex"
-            >
-              <option.Icon className="text-2xl" />
-              <Typography>{option.title}</Typography>
-            </div>
-          ))}
-          <div className="w-[200px] flex justify-between items-center">
-            <Button variant="outlined">Sign up</Button>
-            <Button variant="filled">Sign In</Button>
+      {isFeedPage ? (
+        <>Feed Header</>
+      ) : (
+        <nav className="h-[80px] rounded-xl z-10 dark:bg-[#1b1b1b] bg-white bg-opacity-70 flex items-center px-4 justify-between max-w-screen-2xl mx-auto sticky top-0">
+          {/** Logo dark*/}
+          <div className="hidden dark:md:block">
+            <Image
+              height={35}
+              width={110}
+              src="/images/darkMode-logo.png"
+              alt="Linkedin blue logo"
+            />
           </div>
-        </div>
-      </nav>
+          <div className="hidden dark:hidden md:block">
+            <Image
+              height={65}
+              width={110}
+              src="/images/logo.png"
+              alt="Linkedin blue logo"
+            />
+          </div>
+          {/** Mobile Menu */}
+          <MobileMenu />
+          {/** Desktop options */}
+          <div className="flex items-center md:w-[500px] justify-between">
+            {options.map((option) => (
+              <div
+                key={option.title}
+                className="flex-col items-center justify-center hidden cursor-pointer hover:text-grey-700 md:flex"
+              >
+                <option.Icon className="text-2xl" />
+                <Typography>{option.title}</Typography>
+              </div>
+            ))}
+            <div className="w-[200px] flex justify-between items-center">
+              <Button variant="outlined">Sign up</Button>
+              <Button variant="filled">Sign In</Button>
+            </div>
+          </div>
+        </nav>
+      )}
+      {/** Home Header  */}
+
+      {/** Feed Header show this based on url */}
     </>
   )
 }
