4d7d0bb6e85d37e49a1cf483b797b20730912168
yousaf wazir <umbrellaman612@gmail.com>
2022-07-13 09:20:03 +0100

    ci: added the sub-footer links

--- src/components/static/Footer.tsx
+++ src/components/static/Footer.tsx
@@ -1,3 +1,4 @@
+import { Typography } from '@material-tailwind/react'
 import Image from 'next/image'
 import React from 'react'
 import { FooterLinks } from './FooterLinks'
@@ -38,6 +39,18 @@ export const Footer: React.FC<FooterProps> = () => {
     'Products',
     'Content Topics',
   ]
+  const subFooterLinks = [
+    'About',
+    'User Agreement',
+    'Cookie Policy',
+    'Brand Policy',
+    'Community Guidelines',
+    'Accessibility',
+    'Privacy Policy',
+    'Copyright Policy',
+    'Guest Controls',
+    'Languages',
+  ]
   return (
     <>
       <footer className="flex flex-col items-center p-2 mx-auto max-w-screen-2xl md:flex-row md:h-[400px] md:justify-between">
@@ -57,6 +70,21 @@ export const Footer: React.FC<FooterProps> = () => {
         />
         <FooterLinks title="Directories" linksArray={sectionFourLinks} />
       </footer>
+      {/** Second sub footer */}
+      <div className="h-[60px] max-w-screen-2xl mx-auto flex flex-col items-center">
+        <div className="flex items-center w-[100px] justify-between">
+          <Typography variant="small">Linkedin</Typography>
+          <Typography variant="small">@ 2022</Typography>
+        </div>
+
+        <div className="flex flex-col items-center px-1 mt-2 text-center md:mt-o md:flex-row md:w-full md:justify-between">
+          {subFooterLinks.map((item) => (
+            <>
+              <Typography variant="small">{item}</Typography>
+            </>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
